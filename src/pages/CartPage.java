72e39e6e554d82460f4f6b82071fa6cc4a0b8d08
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 15:55:17 +0530

    Update CartPage.java

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -1,1 +1,20 @@
-<placeholder - already displayed>
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToCart() {
+        driver.findElement(By.cssSelector("#topnav a[title='Cart']")).click();
+    }
+
+    public boolean isProductInCart(String productName) {
+        return driver.getPageSource().contains(productName);
+    }
+}
