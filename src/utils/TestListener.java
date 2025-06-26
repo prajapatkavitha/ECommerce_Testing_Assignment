289116b248566f24adeeacc9b3e6b46ab88976e5
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 16:05:10 +0530

    Update TestListener.java

--- src/utils/TestListener.java
+++ src/utils/TestListener.java
@@ -1,1 +1,49 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        WebDriver driver = DriverFactory.getDriver();
+        if (driver != null) {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File src = ts.getScreenshotAs(OutputType.FILE);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String testName = result.getName();
+            File dest = new File("screenshots/" + testName + "_" + timeStamp + ".png");
+            try {
+                FileUtils.copyFile(src, dest);
+                System.out.println("✔ Screenshot captured: " + dest.getPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override public void onTestStart(ITestResult result) {}
+    @Override public void onTestSuccess(ITestResult result) {}
+    @Override public void onTestSkipped(ITestResult result) {}
+    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
+    @Override public void onStart(ITestContext context) {}
+    @Override public void onFinish(ITestContext context) {}
+}
+
+
+// Add this to testng.xml to activate the listener
+<listeners>
+    <listener class-name="utils.TestListener"/>
+</listeners>
 
