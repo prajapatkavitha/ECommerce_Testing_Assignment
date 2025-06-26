892ebaed4a3ad05adf82010d7e485e91cbe5f1d1
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 15:56:24 +0530

    Update HomeCategoryTest.java

--- src/tests/HomeCategoryTest.java
+++ src/tests/HomeCategoryTest.java
@@ -1,1 +1,28 @@
-<placeholder - already displayed>
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.CategoryPage;
+import pages.HomePage;
+import utils.DriverFactory;
+
+public class HomeCategoryTest {
+    @BeforeClass
+    public void setup() {
+        DriverFactory.getDriver();
+    }
+
+    @Test
+    public void testHomepageCategoryFlow() {
+        HomePage homePage = new HomePage(DriverFactory.getDriver());
+        homePage.navigate();
+        homePage.clickRandomCategory();
+        CategoryPage categoryPage = new CategoryPage(DriverFactory.getDriver());
+        Assert.assertTrue(categoryPage.hasAtLeastThreeProducts(), "Less than 3 products found!");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
