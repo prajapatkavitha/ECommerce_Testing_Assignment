0a3d2374b11844403384159613ea42cbb9b5addf
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 15:53:04 +0530

    Update CSVReaderUtil.java

--- src/utils/CSVReaderUtil.java
+++ src/utils/CSVReaderUtil.java
@@ -1,1 +1,18 @@
-<placeholder - already displayed>
+package utils;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReaderUtil {
+    public static List<String[]> readCSV(String filePath) {
+        List<String[]> data = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            data = reader.readAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
