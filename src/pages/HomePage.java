a1c2653fe4ab965de351a9dcd93b4127388696bc
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 15:53:33 +0530

    Update HomePage.java

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,1 +1,24 @@
-<placeholder - already displayed>
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+import java.util.Random;
+
+public class HomePage {
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigate() {
+        driver.get("https://automationteststore.com/");
+    }
+
+    public void clickRandomCategory() {
+        List<WebElement> categories = driver.findElements(By.cssSelector("#categorymenu li a"));
+        categories.get(new Random().nextInt(categories.size())).click();
+    }
+}
