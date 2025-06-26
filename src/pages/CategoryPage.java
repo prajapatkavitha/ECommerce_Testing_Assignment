0d540c0346ab2d1c10ad1046dd4bc07df79ba98b
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-06-26 15:54:05 +0530

    Update CategoryPage.java

--- src/pages/CategoryPage.java
+++ src/pages/CategoryPage.java
@@ -1,1 +1,18 @@
-<placeholder - already displayed>
+<placeholder - already displayed>package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.util.List;
+
+public class CategoryPage {
+    WebDriver driver;
+
+    public CategoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean hasAtLeastThreeProducts() {
+        List<WebElement> products = driver.findElements(By.cssSelector(".fixed_wrapper .prdocutname"));
+        return products.size() >= 3;
+    }
+}
